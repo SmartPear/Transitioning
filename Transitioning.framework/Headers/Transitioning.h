556ae0db88ec9ccddb64021f5b419429b32b5dc9
王欣 <wangxinobserver@gmail.com>
2019-05-26 15:42:37 +0800

    修改podspec文件

---
+++ Transitioning.framework/Headers/Transitioning.h
@@ -0,0 +1,19 @@
+//
+//  Transitioning.h
+//  Transitioning
+//
+//  Created by 王欣 on 2019/5/26.
+//  Copyright © 2019 王欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Transitioning.
+FOUNDATION_EXPORT double TransitioningVersionNumber;
+
+//! Project version string for Transitioning.
+FOUNDATION_EXPORT const unsigned char TransitioningVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transitioning/PublicHeader.h>
+
+
--- Transitioning.podspec
+++ Transitioning.podspec
@@ -9,7 +9,7 @@ spec.license         = 'MIT'
 spec.author       = { "王欣" => "=573385822@qq.com" }
 spec.platform     = :ios, '10.0'
 spec.source       = { :git => "https://github.com/wangxin1991/Transitioning.git", :tag => "#{spec.version}" }
-spec.source_files = 'Transitioning/**/*.{h,m}'
+spec.vendored_frameworks = 'Transitioning.framework'
 spec.framework  = "UIKit"
 spec.requires_arc = true
 end
